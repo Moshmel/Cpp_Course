4cadf51f29679e0669760ae214edaf3638ec0803
Moshmel <33791298+Moshmel@users.noreply.github.com>
2018-04-12 10:45:24 +0300

    Add files via upload

---
+++ Member.h
@@ -0,0 +1,32 @@
+#include <map>
+#include <iostream>
+#include <vector>
+using namespace std;
+class Member
+{
+
+	char* _name;
+	vector<Member*> _following;
+	vector<Member*> _followers;
+	vector<Member*> _users;
+	static int counter;
+
+
+
+public:
+	~Member();
+
+	Member();
+	Member(char* name);
+	void follow(Member &user);
+	void unfollow(Member &user);
+	int	numFollowers();
+	int	numFollowing();
+
+	static int count()//member count 
+	{
+		return Member::counter;
+	}
+
+};
+
